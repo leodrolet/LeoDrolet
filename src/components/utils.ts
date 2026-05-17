9a605b7d8dd06ca01f51c0096706f65a00eb21a7
Theman3tt3-1 <theman3tt3@gmail.com>
2026-05-17 01:00:50 -0400

    feat: orange color scheme + fix process timeline line

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -27,5 +27,13 @@ export function smoothScrollTo(targetId: string) {
     if (progress < 1) requestAnimationFrame(step);
   }
 
+  document.body.classList.add('is-scrolling');
+
+  function done() {
+    document.body.classList.remove('is-scrolling');
+  }
+
   requestAnimationFrame(step);
+
+  setTimeout(done, duration + 300);
 }
